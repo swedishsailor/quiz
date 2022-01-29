89eb79a98f066897bf289af245dae35880cf3e05
swedishsailor <kzygowski7@gmail.com>
2022-01-29 18:33:32 +0100

    menu and quizView basic layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-console.log('xd');
+var testAnswer = {
+    content: 'south aftica',
+    isCorrect: true,
+    points: 10
+};
+console.log(testAnswer);
+// Query Selectors
+var startButton = document.querySelector('.startButton');
+var informationsButton = document.querySelector('.informationsButton');
+var optionsButton = document.querySelector('.optionsButton');
+var inGameViewTemplate = document.getElementById('inGameViewTemplate');
+var inGameViewTemplateContent = inGameViewTemplate.content;
+// On click Start Button
+startButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    //const copy = inGameViewTemplateContent.cloneNode(true);
+    document.body.innerHTML = '';
+    document.body.appendChild(inGameViewTemplateContent);
+});
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,1 +1,29 @@
-console.log('xd');
+interface Answers {
+    content: string;
+    isCorrect: boolean;
+    points?: number;
+}
+
+
+const testAnswer:Answers = {
+    content: 'south aftica',
+    isCorrect: true,
+    points: 10
+}
+
+console.log(testAnswer);
+
+// Query Selectors
+const startButton:Element = document.querySelector('.startButton');
+const informationsButton:Element = document.querySelector('.informationsButton');
+const optionsButton:Element = document.querySelector('.optionsButton');
+
+const inGameViewTemplate:HTMLTemplateElement = document.getElementById('inGameViewTemplate');
+const inGameViewTemplateContent = inGameViewTemplate.content;
+// On click Start Button
+startButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    //const copy = inGameViewTemplateContent.cloneNode(true);
+    document.body.innerHTML = '';
+    document.body.appendChild(inGameViewTemplateContent);
+})
