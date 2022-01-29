be720247555e1805a7b69a1b4b4411f8bf3b6acf
Kamil Zygowski <kzygowski7@gmail.com>
2022-01-29 13:28:06 +0100

    webpack init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('xd');
+// Test import of a JavaScript module
+import './js/main';
+
+// Test import of an asset
+import webpackLogo from '@/images/webpack-logo.svg'
+
+// Test import of styles
+import '@/styles/index.scss'
