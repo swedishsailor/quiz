db48cc62e14a010158b9727c9e9a1e78333af10f
swedishsailor <kzygowski7@gmail.com>
2022-02-01 19:26:07 +0100

    questions timer and proper quizView rendering

--- src/js/main.js
+++ src/js/main.js
@@ -7,39 +7,47 @@ var QuestionsAndAnswers = /** @class */ (function () {
         this.answers = answers;
         this.points = points;
     }
-    QuestionsAndAnswers.prototype.render = function () {
-        var containerHTML = inGameViewTemplate.innerHTML;
-        // Replace {{Quiz}}
-        document.getElementById('inGameViewTemplate').innerHTML = containerHTML.replace(/{{[A-z]{0,16}}}/g, "" + this.question);
-        var answers = inGameViewTemplateContent.querySelectorAll('#answersButton');
-        console.log(answers);
-        // Replace all {{Answers}}
-        for (var i = 0; i < answers.length; i++) {
-            answers[i].textContent = answers[i].textContent.replace("{{Answer" + (i + 1) + "}}", "" + this.answers[i][0]);
-            //@ts-ignore value is HTML tag elem
-            answers[i].value = "" + this.answers[i][1];
-        }
+    QuestionsAndAnswers.prototype.render = function (HTMLElement, timeLeft) {
+        var newHTML = "    \n        <div id=\"inGameView\">\n        <p class=\"timeLeft\"> Time left: " + timeLeft + " sec</p>\n        <h2>Question</h2>\n        <p class=\"question\">" + this.question + "</p>\n        <div class=\"answers\">\n          <div class=\"leftPanel\">\n            <button value=\"" + this.answers[0][1] + "\" class=\"answer1\" id=\"answersButton\">" + this.answers[0][0] + "</button>\n            <button value=\"" + this.answers[1][1] + "\" class=\"answer2\" id=\"answersButton\">" + this.answers[1][0] + "</button>\n          </div>\n          <div class=\"rightPanel\">\n            <button value=\"" + this.answers[2][1] + "\" class=\"answer3\" id=\"answersButton\">" + this.answers[2][0] + "</button>\n            <button value=\"" + this.answers[3][1] + "\" class=\"answer4\" id=\"answersButton\">" + this.answers[3][0] + "</button>\n          </div>\n        </div>\n      </div>";
+        HTMLElement.innerHTML = newHTML;
     };
     QuestionsAndAnswers.prototype.onClick = function (e) {
         console.log(e);
     };
     return QuestionsAndAnswers;
 }());
-var data;
-var dataReceive = function () { return data_js_1.getData.then(function (result) { data = result; }); };
-dataReceive();
+/**
+ * Functions
+ */
 // After data fetching create every Object and run methods on it
-setTimeout(function () {
-    /*for(let i = 0; i<Object.keys(data).length; i++)
-    {
-        const newQAndA = QuestionsAndAnswers.create(data[i]);
-        console.log(newQAndA);
-    }*/
-    var randomSet = Math.floor(Math.random() * Object.keys(data).length);
-    console.log(data[randomSet]);
-    var test = new QuestionsAndAnswers(data[randomSet].question, data[randomSet].answers, data[randomSet].points);
-    test.render();
-}, 355);
+var randomQuestion = function (HTMLElement) {
+    setTimeout(function () {
+        /*for(let i = 0; i<Object.keys(data).length; i++)
+        {
+            const newQAndA = QuestionsAndAnswers.create(data[i]);
+            console.log(newQAndA);
+        }*/
+        var randomSet = Math.floor(Math.random() * Object.keys(data).length);
+        console.log(data[randomSet]);
+        var test = new QuestionsAndAnswers(data[randomSet].question, data[randomSet].answers, data[randomSet].points);
+        test.render(HTMLElement, 30);
+    }, 355);
+};
+var countTime = function (timeLeft) {
+    var time = setInterval(function () {
+        console.log(timeLeft);
+        timeLeft--;
+        document.querySelector('#inGameView').querySelector('.timeLeft').innerHTML = "Time left: " + timeLeft + " sec";
+        if (timeLeft <= 0) {
+            clearInterval(time);
+            return true;
+        }
+        return false;
+    }, timeLeft * 1000 / 30);
+};
+// Constants and IMPORTANT vars
+var data;
+var QUESTION_TIME = 30;
 // Query Selectors
 var startButton = document.querySelector('.startButton');
 var informationsButton = document.querySelector('.informationsButton');
@@ -47,6 +55,7 @@ var optionsButton = document.querySelector('.optionsButton');
 //@ts-ignore
 var inGameViewTemplate = document.getElementById('inGameViewTemplate');
 var inGameViewTemplateContent = inGameViewTemplate.content;
+var inGameViewCopy = inGameViewTemplate;
 /**
  *  Events
  */
@@ -54,6 +63,7 @@ startButton.addEventListener('click', function (e) {
     e.preventDefault();
     document.body.innerHTML = '';
     document.body.appendChild(inGameViewTemplateContent);
+    countTime(QUESTION_TIME);
 });
 informationsButton.addEventListener('click', function (e) {
     e.preventDefault();
@@ -63,16 +73,33 @@ optionsButton.addEventListener('click', function (e) {
 });
 // On click Quiz answers
 document.addEventListener('click', function (e) {
-    if (e.target.classList.contains('answer1')) {
+    if (e.target.id === "answersButton") {
         console.log(e.target.value);
-    }
-    else if (e.target.classList.contains('answer2')) {
-        console.log(e.target.value);
-    }
-    else if (e.target.classList.contains('answer3')) {
-        console.log(e.target.value);
-    }
-    else if (e.target.classList.contains('answer4')) {
-        console.log(e.target.value);
+        if (e.target.value === 'true') {
+            e.target.classList.add('correct');
+            countTime(5);
+        }
+        else if (e.target.value = 'false') {
+            e.target.classList.add('bad');
+            for (var i = 0; i < document.querySelectorAll('#answersButton').length; i++) {
+                //@ts-ignore
+                if (document.querySelectorAll('#answersButton')[i].value === 'true') {
+                    document.querySelectorAll('#answersButton')[i].classList.add('correct');
+                }
+            }
+            countTime(5);
+        }
+        var randomSet = Math.floor(Math.random() * Object.keys(data).length);
+        var newSet_1 = new QuestionsAndAnswers(data[randomSet].question, data[randomSet].answers, data[randomSet].points);
+        var time = setTimeout(function () {
+            newSet_1.render(document.getElementById('inGameView'), 5);
+            countTime(QUESTION_TIME);
+        }, 5 * 1000);
     }
 });
+/**
+ * INIT MAIN
+ */
+var dataReceive = function () { return data_js_1.getData.then(function (result) { data = result; }); };
+dataReceive();
+randomQuestion(document.getElementById('inGameViewTemplate'));
--- src/js/main.ts
+++ src/js/main.ts
@@ -2,59 +2,80 @@ import { getData } from './data.js';
 
 interface quizData {
     readonly question: string;
-    readonly answers: [];
+    readonly answers: any[];
     readonly points?: number;
-    render: () => void;
+    render: (elem: HTMLElement, timeLeft: number) => void;
 }
 
 class QuestionsAndAnswers implements quizData {
     question: string;
-    answers: [];
+    answers: any[];
     points?: number;
 
-
-    constructor(question: string, answers: [], points: number) {
+    constructor(question: string, answers: any[], points: number) {
         this.question = question;
         this.answers = answers;
         this.points = points;
     }
-
-    render(): void {
-        let containerHTML: string = inGameViewTemplate.innerHTML;
-        // Replace {{Quiz}}
-        document.getElementById('inGameViewTemplate').innerHTML = containerHTML.replace(/{{[A-z]{0,16}}}/g, `${this.question}`);
-
-        const answers = inGameViewTemplateContent.querySelectorAll('#answersButton');
-        console.log(answers)
-        // Replace all {{Answers}}
-        for (let i = 0; i < answers.length; i++) {
-            answers[i].textContent = answers[i].textContent.replace(`{{Answer${i + 1}}}`, `${this.answers[i][0]}`);
-            //@ts-ignore value is HTML tag elem
-            answers[i].value = `${this.answers[i][1]}`;
-        }
+    render(HTMLElement: HTMLElement, timeLeft: number): void {
+        const newHTML: string = `    
+        <div id="inGameView">
+        <p class="timeLeft"> Time left: ${timeLeft} sec</p>
+        <h2>Question</h2>
+        <p class="question">${this.question}</p>
+        <div class="answers">
+          <div class="leftPanel">
+            <button value="${this.answers[0][1]}" class="answer1" id="answersButton">${this.answers[0][0]}</button>
+            <button value="${this.answers[1][1]}" class="answer2" id="answersButton">${this.answers[1][0]}</button>
+          </div>
+          <div class="rightPanel">
+            <button value="${this.answers[2][1]}" class="answer3" id="answersButton">${this.answers[2][0]}</button>
+            <button value="${this.answers[3][1]}" class="answer4" id="answersButton">${this.answers[3][0]}</button>
+          </div>
+        </div>
+      </div>`
+        HTMLElement.innerHTML = newHTML;
     }
     onClick(e: Event): void {
         console.log(e);
     }
 }
 
-let data;
-const dataReceive = () => getData.then(result => { data = result });
-dataReceive();
+
+/**
+ * Functions
+ */
 // After data fetching create every Object and run methods on it
-setTimeout(() => {
-    /*for(let i = 0; i<Object.keys(data).length; i++)
-    {
-        const newQAndA = QuestionsAndAnswers.create(data[i]); 
-        console.log(newQAndA);
-    }*/
+const randomQuestion = (HTMLElement: HTMLElement): void => {
+    setTimeout(() => {
+        /*for(let i = 0; i<Object.keys(data).length; i++)
+        {
+            const newQAndA = QuestionsAndAnswers.create(data[i]); 
+            console.log(newQAndA);
+        }*/
+        const randomSet: number = Math.floor(Math.random() * Object.keys(data).length);
+        console.log(data[randomSet])
+        const test = new QuestionsAndAnswers(data[randomSet].question, data[randomSet].answers, data[randomSet].points);
+        test.render(HTMLElement, 30);
+    }, 355);
+}
 
-    const randomSet: number = Math.floor(Math.random() * Object.keys(data).length);
-    console.log(data[randomSet])
-    const test = new QuestionsAndAnswers(data[randomSet].question, data[randomSet].answers, data[randomSet].points);
-    test.render();
+const countTime = (timeLeft: number):any => {
+    const time = setInterval(() => {
+        console.log(timeLeft)
+        timeLeft--;
+        document.querySelector('#inGameView').querySelector('.timeLeft').innerHTML = `Time left: ${timeLeft} sec`;
+        if (timeLeft <= 0) {
+            clearInterval(time);
+            return true;
+        }
+        return false;
+    }, timeLeft * 1000 / 30)
+}
 
-}, 355);
+// Constants and IMPORTANT vars
+let data: any[];
+const QUESTION_TIME: number = 30;
 
 // Query Selectors
 const startButton: Element = document.querySelector('.startButton');
@@ -65,6 +86,7 @@ const optionsButton: Element = document.querySelector('.optionsButton');
 //@ts-ignore
 const inGameViewTemplate: HTMLTemplateElement = document.getElementById('inGameViewTemplate');
 const inGameViewTemplateContent = inGameViewTemplate.content;
+const inGameViewCopy = inGameViewTemplate;
 /**
  *  Events
  */
@@ -72,6 +94,7 @@ startButton.addEventListener('click', e => {
     e.preventDefault();
     document.body.innerHTML = '';
     document.body.appendChild(inGameViewTemplateContent);
+    countTime(QUESTION_TIME);
 })
 
 informationsButton.addEventListener('click', e => {
@@ -83,19 +106,36 @@ optionsButton.addEventListener('click', e => {
 })
 
 // On click Quiz answers
-document.addEventListener('click', e => {
-    if(e.target.classList.contains('answer1'))
-    {
+document.addEventListener('click', (e: any) => {
+    if (e.target.id === "answersButton") {
         console.log(e.target.value)
-    } else if(e.target.classList.contains('answer2'))
-    {
-        console.log(e.target.value)
-    } else if(e.target.classList.contains('answer3'))
-    {
-        console.log(e.target.value)
-    }else if (e.target.classList.contains('answer4'))
-    {
-        console.log(e.target.value)
-    }
+        if (e.target.value === 'true') {
+            e.target.classList.add('correct');
+            countTime(5);
+        } else if (e.target.value = 'false') {
+            e.target.classList.add('bad');
+            for (let i = 0; i < document.querySelectorAll('#answersButton').length; i++) {
+                //@ts-ignore
+                if (document.querySelectorAll('#answersButton')[i].value === 'true') {
+                    document.querySelectorAll('#answersButton')[i].classList.add('correct');
+                }
+            }
+            countTime(5);
+        }
 
+        const randomSet: number = Math.floor(Math.random() * Object.keys(data).length);
+        const newSet = new QuestionsAndAnswers(data[randomSet].question, data[randomSet].answers, data[randomSet].points);
+        const time = setTimeout(() => {
+            newSet.render(document.getElementById('inGameView'), 5)
+            countTime(QUESTION_TIME);
+        }, 5 * 1000);
+    }
 })
+
+/**
+ * INIT MAIN
+ */
+const dataReceive = () => getData.then(result => { data = result });
+dataReceive();
+
+randomQuestion(document.getElementById('inGameViewTemplate'));
